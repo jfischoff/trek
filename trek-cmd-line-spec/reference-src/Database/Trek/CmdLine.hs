1eb3a338ce68f9df88fe1d1dcfaa790a2faa1afb
Jonathan Fischoff <jfischoff@yahoo.com>
2019-09-28 13:54:48 -0700

    first list test

--- trek-cmd-line-spec/reference-src/Database/Trek/CmdLine.hs
+++ trek-cmd-line-spec/reference-src/Database/Trek/CmdLine.hs
@@ -1,19 +1,9 @@
 module Database.Trek.CmdLine where
 import System.Exit(ExitCode)
 
--- Main API
-setup :: [String] -> IO (ExitCode, String, String)
-setup = undefined
--- Requires setup
-teardown :: [String] -> IO (ExitCode, String, String)
-teardown = undefined
-
 apply :: [String] -> IO (ExitCode, String, String)
 apply = undefined
 
 listApplications :: [String] -> IO (ExitCode, String, String)
 listApplications = undefined
-
-hashConflicts :: [String] -> IO (ExitCode, String, String)
-hashConflicts = undefined
 
--- trek-cmd-line-spec/src/Database/Trek/CmdLineSpec.hs
+++ trek-cmd-line-spec/src/Database/Trek/CmdLineSpec.hs
@@ -25,7 +25,6 @@ aroundAll withFunc specWith = do
 
 data TestMigrations = TestMigrations
   { successfulMigration  :: FilePath
-  , conflictingMigration :: FilePath
   , migrationDirectory   :: FilePath
   , extraMigration       :: FilePath
   }
@@ -36,23 +35,6 @@ makeTestMigrations = undefined
 aroundAllMigration :: SpecWith TestMigrations -> Spec
 aroundAllMigration = aroundAll $
   \f -> withSystemTempDirectory "migration" $ f . makeTestMigrations
-
-noSetupMessage :: (ExitCode, String, String)
-noSetupMessage = (ExitFailure 16, "", "Not Setup! Execute `trek setup` to setup\n")
-
-setupTeardownSpecs :: Spec
-setupTeardownSpecs = do
-  it "setup doesn't accept arguments" $ setup ["hey"] `shouldReturn`
-    (ExitFailure 4, "", "hey is not valid argument for `trek setup`. `trek setup` does not take additional arguments")
-  it "setup initially succeed" $ setup [] `shouldReturn` (ExitSuccess, "", "")
-  it "setup >> setup fails" $ (setup [] >> setup []) `shouldReturn`
-    (ExitFailure 8, "", "Setup Already! >:(\n")
-  it "teardown doesn't accept arguments" $ teardown ["hey"] `shouldReturn`
-    (ExitFailure 4, "", "hey is not valid argument for `trek teardown`. `trek teardown` does not take additional arguments")
-  it "teardown fails without setup" $ teardown [] `shouldReturn` noSetupMessage
-
-  it "teardown succeeds after setup" $ (setup [] >> teardown [])
-    `shouldReturn` (ExitSuccess, "", "")
 
 successfulMigrationRecord :: String
 successfulMigrationRecord = [here|
@@ -108,77 +90,31 @@ bothRecords = [here|
   }
   |]
 
-conflictingMigrationWarning :: String
-conflictingMigrationWarning = "The following versions have hash conflicts [\"12/12/1980\"=\"conflictingHash\"]"
-
 nothingToApply :: String
 nothingToApply = "Nothing to apply!"
 
 applyListApplicationsSpecs :: SpecWith TestMigrations
 applyListApplicationsSpecs = do
-  it "apply without setup fails" $ \TestMigrations {..} -> apply [successfulMigration] `shouldReturn`
-    noSetupMessage
   it "apply returns migration record when successful" $ \TestMigrations {..} -> do
-    _ <- setup []
     apply [successfulMigration] `shouldReturn` (ExitSuccess, successfulMigrationRecord, "")
-  it "reports a hash collision error" $ \TestMigrations {..} -> do
-    _ <- setup []
-    _ <- apply [successfulMigration]
-    apply [conflictingMigration] `shouldReturn` (ExitFailure 32, conflictingMigrationWarning, "")
-  it "reports a hash collision warning" $ \TestMigrations {..} -> do
-    _ <- setup []
-    _ <- apply [successfulMigration]
-    apply ["--warn-hash-conflicts", migrationDirectory] `shouldReturn`
-      (ExitSuccess, extraMigrationRecord, conflictingMigrationWarning)
-  it "reports a hash collision warning" $ \TestMigrations {..} -> do
-    _ <- setup []
-    _ <- apply [successfulMigration]
-    apply ["--no-warn-hash-conflicts", migrationDirectory] `shouldReturn`
-      (ExitSuccess, extraMigrationRecord, "")
   it "migrates more then one in a group" $ \TestMigrations {..} -> do
-    _ <- setup []
     apply [successfulMigration, extraMigration] `shouldReturn`
       (ExitSuccess, bothRecords, "")
-
   it "errors on empty" $ \TestMigrations {..} -> do
-    _ <- setup []
     _ <- apply [successfulMigration]
     apply [successfulMigration] `shouldReturn` (ExitFailure 64, "", nothingToApply)
-
   it "warns on empty" $ \TestMigrations {..} -> do
-    _ <- setup []
     _ <- apply [successfulMigration]
     apply [successfulMigration, "--warn-empty-migration"] `shouldReturn` (ExitSuccess, "", nothingToApply)
-
   it "ignores on empty" $ \TestMigrations {..} -> do
-    _ <- setup []
     _ <- apply [successfulMigration]
     apply [successfulMigration, "--no-warn-empty-migration"] `shouldReturn` (ExitSuccess, "", "")
-
-  it "hash-conflicts fails when not setup" $ \TestMigrations {..} -> do
-    hashConflicts [] `shouldReturn` noSetupMessage
-  it "hash-conflicts returns conflicts" $ \TestMigrations {..} -> do
-    _ <- setup []
-    _ <- apply [successfulMigration]
-    hashConflicts [conflictingMigration] `shouldReturn`
-      (ExitSuccess, "[\"12/12/1980\"=\"conflictingHash\"]", "")
-  it "returns nothing if there are not hash conflicts" $ \TestMigrations {..} -> do
-    _ <- setup []
-    hashConflicts [conflictingMigration] `shouldReturn`
-      (ExitSuccess, "", "")
+  it "list initially returns nothing" $ \TestMigrations {..} ->
+    listApplications [] `shouldReturn` (ExitSuccess, "[]", "")
 
 {-
 
-trek remove-hashes [VERSION]
-exitcode: 16
-stderr: Not Setup! Execute `trek setup` to setup.
 
-trek teardown
-exitcode: 16
-
-In the case that we don't care about hashes
-
-trek migrate FILEPATH
 trek list
 trek teardown
 
@@ -196,5 +132,4 @@ trek teardown
 
 spec :: Spec
 spec = do
-  setupTeardownSpecs
   aroundAllMigration applyListApplicationsSpecs
