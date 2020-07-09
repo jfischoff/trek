ac2de7d7fbb42a17510ba8ed8c207e1b008cadd9
Jonathan Fischoff <jfischoff@yahoo.com>
2020-07-08 23:19:29 -0700

    implement create

--- trek/src/Database/Trek/Parser.hs
+++ trek/src/Database/Trek/Parser.hs
@@ -1,7 +1,24 @@
 module Database.Trek.Parser where
 import Options.Generic
+import Database.PostgreSQL.Simple.PartialOptions
+import qualified Options.Applicative.Builder as B
 
-data Command = Apply FilePath | Create String
+data Command = Apply FilePath PartialOptions | Create String
   deriving (Show, Eq, Generic)
 
-instance ParseRecord Command
+instance ParseRecord Command where
+  parseRecord = do
+    let applyParser
+           =  Apply
+          <$> B.strArgument (B.metavar "FILEPATH")
+          <*> parseRecord
+
+        createParser
+          = Create <$> B.strArgument (B.metavar "NAME")
+
+    B.subparser
+     (  B.command "apply"
+          (B.info applyParser (B.progDesc "Apply migrations"))
+     <> B.command "create"
+          (B.info createParser (B.progDesc "Create migrations"))
+     )
--- trek/src/Database/Trek/Run.hs
+++ trek/src/Database/Trek/Run.hs
@@ -1,1 +1,16 @@
 module Database.Trek.Run where
+import Database.Trek.Db
+import qualified Database.PostgreSQL.Simple.Options as P
+import Data.Time.Format.ISO8601
+import Data.Time
+import System.IO
+
+create :: String -> IO String
+create name = do
+  now <- getCurrentTime
+  let outputFile = name <> "_" <> iso8601Show now
+  withFile outputFile WriteMode (const $ pure ())
+  pure outputFile
+
+apply :: P.Options -> FilePath -> IO (Maybe OutputGroup)
+apply = undefined
